8bc8bba34e47ad13ba2e01f03ee38a59698e5ac9
Emanuel Arispe <ema.arispe@gmail.com>
2023-09-22 22:24:58 -0300

    Se agrega nuevo DER y archivo DB

---
+++ Primera entrega/vinoteca.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2023 at 03:15 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vinoteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bodegas`
+--
+
+CREATE TABLE `bodegas` (
+  `nombre` varchar(45) NOT NULL,
+  `pais` varchar(45) NOT NULL,
+  `provincia` varchar(45) NOT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vinos`
+--
+
+CREATE TABLE `vinos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `bodega` varchar(45) NOT NULL,
+  `anio` int(11) DEFAULT NULL,
+  `maridaje` varchar(45) NOT NULL,
+  `cepa` varchar(45) NOT NULL,
+  `caracteristica` text DEFAULT NULL,
+  `recomendado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bodegas`
+--
+ALTER TABLE `bodegas`
+  ADD PRIMARY KEY (`nombre`),
+  ADD KEY `nombre` (`nombre`);
+
+--
+-- Indexes for table `vinos`
+--
+ALTER TABLE `vinos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bodega` (`bodega`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `vinos`
+--
+ALTER TABLE `vinos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bodegas`
+--
+ALTER TABLE `bodegas`
+  ADD CONSTRAINT `bodegas_ibfk_1` FOREIGN KEY (`nombre`) REFERENCES `vinos` (`bodega`);
+
+--
+-- Constraints for table `vinos`
+--
+ALTER TABLE `vinos`
+  ADD CONSTRAINT `vinos_ibfk_1` FOREIGN KEY (`bodega`) REFERENCES `bodegas` (`nombre`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
